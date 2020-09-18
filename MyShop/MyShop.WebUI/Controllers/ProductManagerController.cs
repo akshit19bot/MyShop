bd2edf9bc7a230ca7bdfc3f9f61ce15748520ab8
Akshit Gungabissoon <akshit.gungabissoon@hangarww.com>
2020-09-18 11:47:21 +0400

    Added Product Upload

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,12 +38,18 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
                 return View(product);
             else
             {
+
+                if(file!= null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -66,7 +73,7 @@ namespace MyShop.WebUI.Controllers
             
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -76,11 +83,16 @@ namespace MyShop.WebUI.Controllers
                 if (!ModelState.IsValid)
                     return View(product);
             }
-
+            if (file != null)
+            {
+                productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+            }
             productToEdit.Category = product.Category;
-            productToEdit.Description = product.Description;
-            productToEdit.Image = product.Image;
+            productToEdit.Description = product.Description;         
+            productToEdit.Name = product.Name;
             productToEdit.Price = product.Price;
+
 
             context.Commit();
             return RedirectToAction("Index");
