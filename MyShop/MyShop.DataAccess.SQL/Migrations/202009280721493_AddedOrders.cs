10eee0045d297e24c7ff29e6b61f9dbb06c6b66f
Akshit Gungabissoon <akshit.gungabissoon@hangarww.com>
2020-09-28 14:50:51 +0400

    Order Models

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202009280721493_AddedOrders.cs
@@ -0,0 +1,42 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedOrders : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Customers", newName: "Orders");
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        OrderId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        ProductName = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Image = c.String(),
+                        Quantity = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId)
+                .Index(t => t.OrderId);
+            
+            AddColumn("dbo.Orders", "OrderStatus", c => c.String());
+            DropColumn("dbo.Orders", "UserId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Orders", "UserId", c => c.String());
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropColumn("dbo.Orders", "OrderStatus");
+            DropTable("dbo.OrderItems");
+            RenameTable(name: "dbo.Orders", newName: "Customers");
+        }
+    }
+}
